7db96051ff5a053c823525fcfb10f169c5141746
Milan Aryal <considermilan@gmail.com>
2021-07-26 10:25:41 +0545

    update

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,10 @@
 module.exports = {
   buildTime: new Date(),
-  title: 'Milan Aryal',
+  title: 'Trading with Milan',
   url: 'https://milanaryal.com.np',
   baseUrl: '/market',
   description:
     'I am providing technical analysis for the financial markets and how they behave based on the Elliott Wave.',
+  author: "Milan Aryal",
+  disclosure: "Disclosure: Please be informed that information I provide is NOT a trading recommendation or investment advice. All of my work is for educational purposes only.",
 }
